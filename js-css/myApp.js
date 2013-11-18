af923a02809506086a3e67c0848b86f93a321a4f
phpusr <phpusr@gmail.com>
2013-11-18 12:58:47 +0600

    add: Слайдеры

--- js-css/myApp.js
+++ js-css/myApp.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', ['ngSanitize']);
 app.controller('MyCtrl', function MyCtrl($scope) {
-    var ctx;
+    var ctx = document.getElementById("myCanvas").getContext("2d");
+    $('div.slider').slider({
+        range: 'max', min: 0, max: 10, value: 2
+    });
 
     function drawLine(x1, y1, x2, y2) {
         ctx.beginPath();
@@ -10,13 +13,21 @@ app.controller('MyCtrl', function MyCtrl($scope) {
         ctx.stroke();
     }
 
-    var MAX_X = MAX_Y = 500;
-    var MIN_X = MIN_Y = 0;
+    $scope.changeSlider = function() {
+        $scope.valX = $('#sliderX').slider('value');
+        $scope.valY = $('#sliderY').slider('value');
+        $scope.valZ = $('#sliderZ').slider('value');
+    };
+
+    //Константы
+    var MAX_X = 500, MAX_Y = MAX_X;
+    var MIN_X = 0, MIN_Y = MIN_X;
     var X0 = MAX_X/2, Y0 = MAX_Y/2;
 
-    ctx = document.getElementById("myCanvas").getContext("2d");
+    //Рисование осей
     drawLine(MIN_X, MIN_Y, MAX_X, MAX_Y);
     drawLine(MAX_X/2, MIN_Y, MAX_X/2, MAX_Y);
     drawLine(MIN_X, MAX_Y/2, MAX_X, MAX_Y/2);
 
+    $scope.changeSlider();
 });
