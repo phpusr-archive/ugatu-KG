1bd87f12e1be3a4e0442db0ff556139bf22c9b1c
phpusr <phpusr@gmail.com>
2013-11-18 17:06:48 +0600

    add: Вывод отрезка A1A2

--- js-css/myApp.js
+++ js-css/myApp.js
@@ -6,10 +6,23 @@ app.controller('MyCtrl', function MyCtrl($scope) {
     var MAX_X = 500, MAX_Y = MAX_X;
     var MIN_X = 0, MIN_Y = MIN_X;
     var X0 = MAX_X/2, Y0 = MAX_Y/2;
+    var SCALE = 20;
 
-    //Инициализация слайдеров
-    $('div.slider').slider({
-        range: 'max', min: 0, max: 10, value: 2,
+    //Инициализация слайдеров (III-октант)
+    $('#sliderX').slider({
+        range: 'max', min: 0, max: 10, value: 0,
+        change: function(event, ui) {
+            changeSlider();
+        }
+    });
+    $('#sliderY').slider({
+        range: 'max', min: -10, max: 0, value: 0,
+        change: function(event, ui) {
+            changeSlider();
+        }
+    });
+    $('#sliderZ').slider({
+        range: 'max', min: -10, max: 0, value: 0,
         change: function(event, ui) {
             changeSlider();
         }
@@ -52,28 +65,59 @@ app.controller('MyCtrl', function MyCtrl($scope) {
         drawText('X', MIN_X, Y0);
         drawText('-X', MAX_X-size, Y0);
 
-        drawText('Z', X0, MAX_Y);
-        drawText('-Z', X0, MIN_Y+size);
+        drawText('Z', X0, MIN_Y+size);
+        drawText('-Z', X0, MAX_Y);
 
-        drawText('-Y', MIN_X+size, MIN_Y+size);
         drawText('Y', MAX_X-size, MAX_Y-size);
+        drawText('-Y', MIN_X+size, MIN_Y+size);
     }
 
-    var pointX = 0, pointY = 0;
+    var pX = 0, pY = 0;
 
     function changeSlider() {
-        $scope.valX = $('#sliderX').slider('value');
-        $scope.valY = $('#sliderY').slider('value');
-        $scope.valZ = $('#sliderZ').slider('value');
+        clearCanvas();
 
-        pointX = X0 - ($scope.valX - Math.round($scope.valY * Math.cos(45))) * 30;
-        pointY = Y0 - ($scope.valZ - Math.round($scope.valY * Math.cos(45))) * 30;
+        var valX = $('#sliderX').slider('value');
+        var valY = $('#sliderY').slider('value');
+        var valZ = $('#sliderZ').slider('value');
+
+        pX = getX(valX, valY);
+        pY = getY(valZ, valY);
+        console.log('pX=', pX, 'pY=', pY);
+
+        //Проекция точки A
+        var a1x = getX(valX, valY);
+        var a1y = getX(0, valY);
+        console.log('a1x=', a1x, 'a1y=', a1y);
+
+        var a2x = getX(0, valY);
+        var a2y = getX(valZ, valY);
+        console.log('a2x=', a2x, 'a2y=', a2y);
+
+        drawPoint(a1x, a1y);
+        drawText('A1', a1x, a1y);
+        drawPoint(a2x, a2y);
+        drawText('A2', a2x, a2y);
+        drawLine(a1x, a1y, a2x, a2y);
+
+
+        $scope.valX = valX;
+        $scope.valY = valY;
+        $scope.valZ = valZ;
         if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
             $scope.$apply();
         }
 
-        clearCanvas();
-        drawPoint(pointX, pointY);
+
+        drawPoint(pX, pY);
+    }
+    /** X - координата в 2D */
+    function getX(x3D, y3D) {
+        return X0 - (x3D - Math.round(y3D * Math.cos(45))) * SCALE
+    }
+    /** Y - координата в 2D */
+    function getY(z3D, y3D) {
+        return Y0 - (z3D - Math.round(y3D * Math.sin(45))) * SCALE
     }
 
     //Start
